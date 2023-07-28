96149546a3e244de63a213e4f5c37ef5d352580e
YarramsettyRamyasri <126143858+YarramsettyRamyasri@users.noreply.github.com>
2023-07-28 16:01:16 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,52 @@
+ReactDOM.render(
+    <div>
+       <h1>ROHIT SHARMA</h1>
+       <p>Rohit Gurunath Sharma (born 30 April 1987), is an Indian international cricketer and the current captain
+         of India men’s cricket team in all formats.Considered one of the best batsmen of his generation and one of
+        greatest opening batters of all time,[3] Sharma is known for his timing,elegance, six-hitting abilities and 
+        leadership skills. He plays as a right-handed batsman for India national cricket team in international cricket,
+         Mumbai Indians in IPL and for Mumbai in domestic cricket. Rohit also captains Mumbai Indians and the team has
+          won 5 titles under his leadership, the most by any team. With India,Sharma was a member of the team that won
+        the 2007 T20 World Cup, and the 2013 ICC Champions Trophy,where he played in the finals of both tournaments.</p>
+        <h1>Early Life</h1>
+        <p >Sharma was born on 30 April 1987 in Bansod, Nagpur, Maharashtra.[4] His mother, Purnima Sharma, is from Visakhapatnam,
+             Andhra Pradesh.[5] His father, Gurunath Sharma, worked as a caretaker of a transport firm storehouse. Sharma was 
+             raised by his grandparents and uncles in Borivali because of his father's low income. He would visit his parents, 
+             who lived in a single-room house in Dombivli, only during weekends.[6] He has a younger brother, Vishal Sharma.[7]
+             Sharma joined a cricket camp in 1999 with his uncle's money. Dinesh Lad, his coach at the camp,
+             Sharma recollects, "I told him I couldn't afford it, but he got me a scholarship. 
+             So for four years I didn't pay a penny, and did well in my cricket".[7] Sharma started as an off-spinner who could 
+             bat a bit before Lad noticed his batting ability and promoted him from number eight to open the innings. He excelled 
+             in the Harris and Giles Shield school cricket tournaments, "scoring a century on debut as an opener".[8]</p>
+      <img src="https://images.lifestyleasia.com/wp-content/uploads/sites/7/2022/10/17150416/rohitsharma45_299315602_121365493977560_2553009705661813473_n-e1666002211593.jpg?tr=w-1200,h-900" hegiht="400px" width="400px"></img>
+      <h2>Matches He played for India</h2>
+      <ul>
+            <li>Test matches</li>
+            <li>2015 and 2019 Cricket World Cups</li>
+            <li>Other one-day international matches</li>
+            <li>Twenty20 international matches</li>
+            <li>Indian Premier League</li>
+        </ul>
+      <p>In November 2013, during Sachin Tendulkar's farewell series, Sharma made his Test debut at Eden Gardens in Kolkata 
+        against West Indies and scored 177, the second-highest score on debut by an Indian to Shikhar Dhawan (187).[17] He 
+        followed it up with 111 (not out) in the second Test at his home ground, the Wankhede Stadium in Mumbai.[18]</p>
+        <h1>Acievements</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/The_Minister_of_State_for_Youth_Affairs_and_Sports_%28IC%29%2C_Water_Resources%2C_River_Development_and_Ganga_Rejuvenation%2C_Shri_Vijay_Goel_conferring_the_Arjuna_Award_on_Cricketer_Rohit_Sharma%2C_in_New_Delhi_on_September_16%2C_2016.jpg/330px-thumbnail.jpg"></img>
+        <ul>
+            <li>2015 – Arjuna Award[102]</li>
+            <li>2020 – Major Dhyan Chand Khel Ratna[103]</li>
+            <li>ICC Men's ODI Cricketer of the Year: 2019[104]</li>
+            <li>ICC Men's ODI Team of the Year: 2014 (12th man),[105] 2016,[106] 2017,[107] 2018,[108] 2019[109]</li>
+            <li>ICC Men's ODI Team of the Decade: 2011–2020[110]</li>
+            <li>ICC Men's T20I Team of the Decade: 2011–2020[110]</li>
+            <li>ICC Men's Test Team of the Year: 2021[111]</li>
+        </ul>
+        <h1>PersonalLife</h1>
+        <p>Sharma married his longtime girlfriend, Ritika Sajdeh on 13 December 2015. They have one child,
+             a girl born on 30 December 2018.[113] He is a practitioner of the meditation technique Sahaj 
+             Marg.[114]He practices an Eggetarian diet [115] but is known to have eaten beef at least 
+             once since.[116]</p>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/f/f5/Rohit_Sharma_with_Ritika_Sajdeh_%28cropped%29.jpg"></img>
+       </div>   
+       , 
+       document.getElementById("content"));
